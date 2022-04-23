89f095604c107bfefb576086ea75c2aea4ab680d
lorenzoh <lorenz.ohly@gmail.com>
2022-04-23 16:41:56 +0200

    Files generated by PkgTemplates

---
+++ src/PollenExample.jl
@@ -0,0 +1,5 @@
+module PollenExample
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PollenExample
+using Test
+
+@testset "PollenExample.jl" begin
+    # Write your tests here.
+end
